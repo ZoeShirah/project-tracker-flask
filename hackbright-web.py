2c72384ac136cb30ff8ccb7fe3385450b43846f1
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-24 17:55:22 -0800

    regular section complete

---
+++ hackbright-web.py
@@ -0,0 +1,97 @@
+from flask import Flask, request, render_template, flash, redirect
+import hackbright
+
+app = Flask(__name__)
+app.secret_key = 'some_secret'
+
+
+@app.route("/student")
+def get_student():
+    """Show information about a student."""
+
+    github = request.args.get('github', 'jhacks')
+    first, last, github = hackbright.get_student_by_github(github)
+    grades = hackbright.get_grades_by_github(github)
+    html = render_template("student_info.html",
+                           first=first,
+                           last=last,
+                           github=github,
+                           grades=grades)
+
+    return html
+
+
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("student_search.html")
+
+
+@app.route("/student-add")
+def add_student_form():
+    """Show form for adding a student."""
+
+    return render_template("student_add.html")
+
+
+@app.route("/student-add", methods=['POST'])
+def add_student():
+    """Show added student."""
+
+    fname = request.form.get('fname')
+    lname = request.form.get('lname')
+    github = request.form.get('github')
+
+    hackbright.make_new_student(fname, lname, github)
+
+    flash('%s %s Succesfully Added' % (fname, lname))
+
+    return redirect("/student?github="+github)
+
+
+@app.route("/project")
+def project_info():
+    """Show all projects and all students who completed them"""
+
+    title = request.args.get('title')
+    p_title, description, max_grade = hackbright.get_project_by_title(title)
+
+    grades = hackbright.get_grades_by_title(title)
+
+    dic = {}
+
+    for grade in grades:
+        github = grade[0]
+        grd = grade[1]
+        first, last, github = hackbright.get_student_by_github(github)
+        grade = (first, last, grd)
+        dic[github] = grade
+
+    print dic
+
+
+    # first, last, github = hackbright.get_student_by_github(github)
+
+    return render_template("project_info.html",
+                           title=title,
+                           description=description,
+                           max_grade=max_grade,
+                           grades=dic
+                           )
+
+@app.route("/")
+def homepage():
+    """Shows list of all projects and all students"""
+
+    students = hackbright.get_all_students()
+    projects = hackbright.get_all_projects()
+
+    return render_template("homepage.html",
+                           students=students,
+                           projects=projects)
+
+
+if __name__ == "__main__":
+    hackbright.connect_to_db(app)
+    app.run(debug=True)
